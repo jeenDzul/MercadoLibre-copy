84a5a2c032b742cac33416b9e122434f62df26f0
jeenDzul <jeen.dzul@meru.com>
2022-10-09 01:05:36 -0500

    add service api to call in function axios

--- services/public-service.ts
+++ services/public-service.ts
@@ -4,11 +4,13 @@ import products from "mock/products";
 import { AxiosCall } from "models/axios-call.model";
 import ItemsIProps from "models/response/items.response.model";
 import product from "mock/product";
+import axios from 'axios';
 
 
 export const fetchProducts = (query?: string): AxiosCall<ItemsIProps> => {
     const controller = loadAbort();
-    return { call: Promise.resolve({ data: products } as AxiosResponse,), controller: controller };
+    return { call: axios.get<ItemsIProps>(`/api/items?q=${query}`), controller: controller };
+    //return { call: Promise.resolve({ data: products } as AxiosResponse,), controller: controller };
 }
 
 
