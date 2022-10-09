3d04f0442b21e962cd2cb9698678d9b4499d7306
jeenDzul <jeen.dzul@meru.com>
2022-10-09 11:06:46 -0500

    add: get categories from redux

--- pages/items/[productId]/hooks/useProductDetail.ts
+++ pages/items/[productId]/hooks/useProductDetail.ts
@@ -2,16 +2,20 @@ import { useCallback, useEffect, useState } from "react";
 import createProductAdapter, { ProductDetailInterface } from "adapters/fetch.product.detail.adapter";
 import useLoadFetch from "hooks/useLoadFetch";
 import { fetchProductDetail } from "services/public-service";
+import { useSelector } from "react-redux";
+import { AppStoreInterface } from "redux/store";
 
 const useProductDetail = (productId) => {
 
     const { loading, callEndpoint } = useLoadFetch();
+    const categories = useSelector((store: AppStoreInterface) => store.products.categories);
+
     const [productDetailEntity, setProductDetailEntity] = useState<ProductDetailInterface>({});
 
     const fetch = useCallback(async () => {
         const response = await callEndpoint(fetchProductDetail(productId));
         const detailEntity = createProductAdapter(response);
-        setProductDetailEntity(detailEntity);
+        setProductDetailEntity({ ...detailEntity, categories });
     }, [productId]);
 
 
